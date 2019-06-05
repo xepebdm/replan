7710a39e6689b0d7f7edb53b4a25cc7b57611dbb
Alexsandre Rodrigues <dev.alex.rodrigues@gmail.com>
2019-06-05 17:48:52 -0300

    Commit inicial

---
+++ src/main/java/com/tim/replan/ReplanApplication.java
@@ -0,0 +1,13 @@
+package com.tim.replan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplanApplication.class, args);
+	}
+
+}
